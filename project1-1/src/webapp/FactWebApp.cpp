913bda9e6bb2596b32ce6123f11c52c56046dbab
isaias <isaias.alfaro@ucr.ac.cr>
2024-09-28 13:20:01 -0600

    Mostrar resultados en WebApp

--- project1-1/src/webapp/FactWebApp.cpp
+++ project1-1/src/webapp/FactWebApp.cpp
@@ -6,10 +6,13 @@
 #include <regex>
 #include <stdexcept>
 #include <string>
+#include <vector>
+#include <sstream>
 
 #include "FactWebApp.hpp"
 #include "HttpRequest.hpp"
 #include "HttpResponse.hpp"
+#include "FactCal.hpp"
 
 FactWebApp::FactWebApp() {
 }
@@ -83,27 +86,77 @@ bool FactWebApp::serveFactorization(HttpRequest& httpRequest
   // TODO(you): URI can be a multi-value list, e.g: 100,2784,-53,200771728
   // TODO(you): Use arbitrary precision for numbers larger than int64_t
   // TODO(you): Modularize this method
+  FactCal Calculator;
   std::smatch matches;
-  std::regex inQuery("^/fact(/|\\?number=)(\\d+)$");
+  std::regex inQuery("^/fact(/|\\?number=)([\\d,]+)$");
   if (std::regex_search(httpRequest.getURI(), matches, inQuery)) {
     assert(matches.length() >= 3);
+
     const int64_t number = std::stoll(matches[2]);
+    std::vector<int64_t> numbers;
+    std::stringstream ss(std::to_string(number));
+    std::string auxiliar = "";
+    std::stringstream body;
+
+    while (std::getline(ss, auxiliar, ',')) {
+        numbers.push_back(std::stoi(auxiliar));
+    }
+
+    // Construccion Respuesta
+    std::string title = "Prime factorization of " + std::to_string(number);
+    body << "<h1>" << title << "</h1>\n";
+    for (int64_t i = 0; i <= numbers.size(); i++) {
+      Calculator.Calculator_Factorial(numbers[i]);
+      if (Calculator.get_Factorial() == "false") {
+        body << "  <h2 class=\"err\">" << numbers[i] << "</h2>\n"
+             << "  <p>" << numbers[i] << ": invalid number</p>\n";
+      } else {
+
+        int64_t startPos = 0;
+        int64_t spacePos;
+        std::string numero, potency;
+
+        while ((spacePos = Calculator.get_Factorial().
+            find(" ", startPos)) != std::string::npos) {
+            // Extraer el primer número (number)
+            numero = Calculator.get_Factorial().
+                substr(startPos, spacePos - startPos);
+
+            // Actualizar startPos para apuntar a la siguiente posición
+            startPos = spacePos + 1;
+
+            // Encontrar la siguiente posición de espacio para extraer "potency"
+            spacePos = Calculator.get_Factorial().find(" ", startPos);
+
+            // Verificar si se encontró otro espacio (si no, es la última parte)
+            if (spacePos != std::string::npos) {
+                // Extraer la potencia (potency)
+                potency = Calculator.get_Factorial().
+                    substr(startPos, spacePos - startPos);
+
+                // Actualizar startPos nuevamente para continuar
+                startPos = spacePos + 1;
+            } else {
+                // extraer el último valor como "potency"
+                potency = Calculator.get_Factorial().substr(startPos);
+                startPos = Calculator.get_Factorial().length();
+            }
+
+            // Imprimir o procesar la pareja number y potency
+            httpResponse.body() << "<p>" << numero << "<sup>"
+                << potency << "</sup></p>";
+        }
+      }
+    }
 
     // TODO(you): Factorization must not be done by factorization threads
     // Build the body of the response
-    std::string title = "Prime factorization of " + std::to_string(number);
     httpResponse.body() << "<!DOCTYPE html>\n"
       << "<html lang=\"en\">\n"
       << "  <meta charset=\"ascii\"/>\n"
-      << "  <title>" << title << "</title>\n"
+      << "  <title>Factorization Results</title>\n"
       << "  <style>body {font-family: monospace} .err {color: red}</style>\n"
-      << "  <h1>" << title << "</h1>\n"
-      << "  <h2>200</h2>\n"
-      << "  <p>200 = 2<sup>3</sup> 5<sup>2</sup></p>\n"
-      << "  <h2 class=\"err\">-3</h2>\n"
-      << "  <p>-3: invalid number</p>\n"
-      << "  <h2>13</h2>\n"
-      << "  <p>-13 is prime</p>\n"
+      << body.str()
       << "  <hr><p><a href=\"/\">Back</a></p>\n"
       << "</html>\n";
   } else {
