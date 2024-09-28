a437b057ddea6f094c392a447524094f62ae5a51
Aaron Santana Valdelomar <84732450+Santaval@users.noreply.github.com>
2024-09-28 16:44:54 -0500

    Refactor HttpConnectionHandler to handle multiple applications

--- project1-1/src/http/HttpConnectionHandler.cpp
+++ project1-1/src/http/HttpConnectionHandler.cpp
@@ -1,17 +1,78 @@
 #include "HttpConnectionHandler.hpp"
+#include "Socket.hpp"
 #include "Log.hpp"
+#include "NetworkAddress.hpp" // Include the header for NetworkAddress
 
-HttpConnectionHandler::HttpConnectionHandler() {
+HttpConnectionHandler::HttpConnectionHandler(std::vector<HttpApp*> applications) {
+    this->applications = applications;
 }
 
 void HttpConnectionHandler::consume(Socket socket) {
-    // this->handleClientConnection(socket);
-    Log::append(Log::INFO, "webserver", "Hola Socket");
-
+    this->handleClientConnection(socket);
     return;
 }
 
 int HttpConnectionHandler::run () {
     this->consumeForever();
     return 0;
+}
+
+void HttpConnectionHandler::handleClientConnection(Socket& client) {
+    while (true) {
+    // Create an object that parses the HTTP request from the socket
+    HttpRequest httpRequest(client);
+
+    // If the request is not valid or an error happened
+    if (!httpRequest.parse()) {
+      // Non-valid requests are normal after a previous valid request. Do not
+      // close the connection yet, because the valid request may take time to
+      // be processed. Just stop waiting for more requests
+      break;
+    }
+
+    // A complete HTTP client request was received. Create an object for the
+    // server responds to that client's request
+    HttpResponse httpResponse(client);
+
+    // Give subclass a chance to respond the HTTP request
+    const bool handled = this->handleHttpRequest(httpRequest, httpResponse);
+
+    // If subclass did not handle the request or the client used HTTP/1.0
+    if (!handled || httpRequest.getHttpVersion() == "HTTP/1.0") {
+      // The socket will not be more used, close the connection
+      client.close();
+      break;
+    }
+  }
+}
+
+bool HttpConnectionHandler::handleHttpRequest(HttpRequest& httpRequest,
+    HttpResponse& httpResponse) {
+  // Print IP and port from client
+  const NetworkAddress& address = httpRequest.getNetworkAddress();
+  Log::append(Log::INFO, "connection",
+    std::string("connection established with client ") + address.getIP()
+    + " port " + std::to_string(address.getPort()));
+
+  // Print HTTP request
+  Log::append(Log::INFO, "request", httpRequest.getMethod()
+    + ' ' + httpRequest.getURI()
+    + ' ' + httpRequest.getHttpVersion());
+
+  return this->route(httpRequest, httpResponse);
+}
+
+
+bool HttpConnectionHandler::route(HttpRequest& httpRequest, HttpResponse& httpResponse) {
+  // Traverse the chain of applications
+  for (size_t index = 0; index < this->applications.size(); ++index) {
+    // If this application handles the request
+    HttpApp* app = this->applications[index];
+    if (app->handleHttpRequest(httpRequest, httpResponse)) {
+      return true;
+    }
+  }
+
+  // Unrecognized request
+  // return this->serveNotFound(httpRequest, httpResponse);
 }
--- project1-1/src/http/HttpConnectionHandler.hpp
+++ project1-1/src/http/HttpConnectionHandler.hpp
@@ -1,5 +1,8 @@
 #include "Consumer.hpp"
 #include "Socket.hpp"
+#include "HttpApp.hpp"
+#include "HttpRequest.hpp"
+#include "HttpResponse.hpp"
 
 #ifndef HTTPCONNECTIONHANDLER_H
 #define HTTPCONNECTIONHANDLER_H
@@ -8,15 +11,21 @@ class HttpConnectionHandler : public Consumer<Socket> {
 
     DISABLE_COPY(HttpConnectionHandler);
 
+    protected:
+      std::vector<HttpApp*> applications;
+
     public:
       /// Constructor
-      explicit HttpConnectionHandler();
+      explicit HttpConnectionHandler(std::vector<HttpApp*> applications);
       int run() override;
       /// Override this method to process any data extracted from the queue
       void consume(Socket data) override;
+      void handleClientConnection(Socket& client);
+      bool handleHttpRequest(HttpRequest& httpRequest,
+      HttpResponse& httpResponse);
+      bool route(HttpRequest& httpRequest, HttpResponse& httpResponse);
 
-    
-
+  
 };
 
 #endif
