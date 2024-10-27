a0b352e97002da89a7ce0727aeaa8565d53b7193
Aaron Santana Valdelomar <84732450+Santaval@users.noreply.github.com>
2024-10-27 01:36:07 -0500

    Refactor Packer class to consume Calculator and produce HttpPendingRequest

---
+++ project1-1/src/productionLine/Packer.cpp
@@ -0,0 +1,17 @@
+#include "Packer.hpp"
+
+void Packer::consume(Calculator* calculator) {
+  HttpPendingRequest* pendingRequest = calculator->getPendingRequest();
+  pendingRequest->increaseProcessedCount();
+
+  if (pendingRequest->getProcessedCount() == pendingRequest->getNumbers().size()) {
+    this->produce(pendingRequest);
+  } else {
+    delete calculator;
+  }
+}
+
+int Packer::run() {
+  this->consumeForever();
+  return 0;
+}
---
+++ project1-1/src/productionLine/Packer.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../prodcons/Assembler.hpp"
+#include "Calculator.hpp"
+#include "../http/HttpPendingRequest.hpp"
+
+class Packer : public Assembler<Calculator*, HttpPendingRequest*> {
+  DISABLE_COPY(Packer);
+
+ public:
+  explicit Packer(Queue<Calculator*>* calculatorQueue = nullptr
+    , Queue<HttpPendingRequest*>* httpPendingRequestQueue = nullptr
+    , Calculator* stopCondition = nullptr)
+    : Assembler<Calculator*, HttpPendingRequest*>(calculatorQueue, httpPendingRequestQueue, stopCondition) {
+  }
+
+  virtual ~Packer() {
+  }
+
+ protected:
+    /// @brief Consume a Calculator and produce a HttpPendingRequest
+    /// @param Calculator 
+    virtual void consume(Calculator* Calculator) override;
+
+    /// @brief Override of run method from Thread
+    virtual int run() override;
+};  
