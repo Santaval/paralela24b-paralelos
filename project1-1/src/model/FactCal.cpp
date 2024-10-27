34538c824f9bcd873a5454173ad33435aac96026
isaias <isaias.alfaro@ucr.ac.cr>
2024-10-27 01:46:24 -0600

    Now the models return a vector with results

--- project1-1/src/model/FactCal.cpp
+++ project1-1/src/model/FactCal.cpp
@@ -8,16 +8,13 @@ FactCal::FactCal() {
 FactCal::~FactCal() {
 }
 
-std::vector <int64_t> FactCal::get_Factorial() {
-  return this->Factorial;
-}
 
-void FactCal::Calculator_Factorial(int64_t number) {
+std::vector<int64_t> FactCal::Calculator_Factorial(int64_t number) {
   int64_t first = number;
   std::vector<int64_t> factors;
-  this->Factorial.clear();
   if (number < 2) {
-    this->Factorial.push_back(0);
+    factors.push_back(0);
+    return factors;
   } else {
     while (first > 1) {
       for (int64_t i = 2; i <= first; i++) {
@@ -28,18 +25,20 @@ void FactCal::Calculator_Factorial(int64_t number) {
         }
       }
     }
-    FactCal::Format_numbers(factors);
+    return (FactCal::Format_numbers(factors));
   }
 }
 
-void FactCal::Format_numbers(std::vector<int64_t> factors) {
+std::vector<int64_t> FactCal::Format_numbers(std::vector<int64_t> factors) {
+  std::vector<int64_t> Factorial;
   while (!factors.empty()) {
     int64_t current_factor = factors[0];
-    this->Factorial.push_back(current_factor);
+    Factorial.push_back(current_factor);
     int count = std::count(factors.begin(), factors.end(), current_factor);
-    this->Factorial.push_back(count);
+    Factorial.push_back(count);
     factors.erase(std::remove(factors.begin(),
         factors.end(), current_factor), factors.end());
   }
+  return Factorial;
 }
 
