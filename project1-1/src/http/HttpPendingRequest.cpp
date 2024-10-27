5696fd9c83f93e3ae2d7185a49be06759d4b2a4b
Aaron Santana Valdelomar <84732450+Santaval@users.noreply.github.com>
2024-10-27 05:19:56 -0500

    Refactor HttpFactPendingRequest to use sendResponse() instead of buildResponse()

--- project1-1/src/http/HttpPendingRequest.cpp
+++ project1-1/src/http/HttpPendingRequest.cpp
@@ -1,10 +1,12 @@
 
 #include "HttpPendingRequest.hpp"
+#include "Log.hpp"
 
 void HttpPendingRequest::pushNUmber(int number) {
     this->numbers.push_back(number);
 }
 
 void HttpPendingRequest::pushResult(int index, std::vector<int64_t> result) {
-    results[index] = result;
+    results.push_back(result);
+
 }
--- project1-1/src/productionLine/CalculatorWorker.cpp
+++ project1-1/src/productionLine/CalculatorWorker.cpp
@@ -12,4 +12,5 @@ void CalculatorWorker::consume(Calculator* Calc) {
   int number = Calc->getPendingRequest()->getNumbers()[index];
   Calc->getPendingRequest()->pushResult(index, Calc->calculate(number));
   this->produce(Calc);
+
 }
