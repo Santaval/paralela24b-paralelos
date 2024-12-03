7f828f3b78c0e25505d18b91a3727df70e6bc326
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-12-03 17:32:12 -0600

    Enhance CalcAssembler to log parsed client requests and improve request handling

--- project1-1/src/productionLine/CalcAssembler.cpp
+++ project1-1/src/productionLine/CalcAssembler.cpp
@@ -28,6 +28,9 @@ int CalcAssembler::run() {
 
 void CalcAssembler::handleClientConnection(Socket& client) {
   CalcRequest request = this->parseRequestLine(client);
+  // log the request
+
+
   if (request.number != -1) {
     Calculator* calculator = this->assembleCalculator(request);
     this->produce(calculator);
@@ -41,10 +44,27 @@ CalcRequest CalcAssembler::parseRequestLine(Socket& client) {
       Log::append(Log::ERROR, "CalcAssembler", "Failed to read from client");
       break;
     }
-    Log::append(Log::INFO, "CalcAssembler", "Received line: " + requestLine);
+
+    // line format: "type:string,pendingRequest:HttpPendingRequest*,numberIndex:int,number:int"
+
     CalcRequest calcRequest;
-    scanf(requestLine.c_str(), "%s,%p,%d,%d", calcRequest.type.c_str(), &calcRequest.pendingRequest,
-      &calcRequest.numberIndex, &calcRequest.number);
+    std::istringstream lineStream(requestLine);
+    std::string pendingRequest, numberIndex, number;
+
+    std::getline(lineStream, calcRequest.type, ',');
+    std::getline(lineStream, pendingRequest, ',');
+    std::getline(lineStream, numberIndex, ',');
+    std::getline(lineStream, number, ',');
+
+    calcRequest.numberIndex = std::stoi(numberIndex);
+    calcRequest.number = std::stoi(number);
+    // Log the request
+    Log::append(Log::INFO, "CalcAssembler", "Request: " + calcRequest.type + ", " + pendingRequest + ", " + std::to_string(calcRequest.numberIndex) + ", " + std::to_string(calcRequest.number));
+
+
+    // convert the string to a pointer
+    calcRequest.pendingRequest = reinterpret_cast<HttpPendingRequest*>
+      (std::stoll(pendingRequest));
     return calcRequest;
   }
   CalcRequest calcRequest;
