fdacf2dd9412efc499cb061aa6f8713d780b544c
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-12-07 20:46:48 -0600

    Implement job loading from file and update main simulation loop for dynamic particle management

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,15 +1,16 @@
 // Copyriht Aaron Josue Santana Valdelomar - Universidad de Costa Rica 2024
-#pragma once
 #include <SFML/Graphics.hpp>
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <string>
 
 #include "Particle.hpp"
 #include "defines.hpp"
 #include "Universe.hpp"
-#include <unistd.h>
-#include <iostream>
 
-#define DISPLAY_SCALE 0.2
-
+#define DISPLAY_SCALE 2
 void renderParticles(sf::RenderWindow& window, const std::vector<Particle*>& particles) {
   // Clear the window
   window.clear();
@@ -25,7 +26,6 @@ void renderParticles(sf::RenderWindow& window, const std::vector<Particle*>& par
     }
     sf::CircleShape shape(particle->getRadio()); // Use radius for simplicity
     shape.setPosition(center.x + particle->getPosition().x / DISPLAY_SCALE, center.y - particle->getPosition().y / DISPLAY_SCALE);
-    
     // add label with velocity
     sf::Font font;
     font.loadFromFile("/home/savaldev/Documents/ucr/paralela24b-paralelos/project2/Montserrat-Regular.ttf");
@@ -84,27 +84,58 @@ void renderParticles(sf::RenderWindow& window, const std::vector<Particle*>& par
   window.display();
 }
 
+std::vector<JobData> loadJobsFromFile(const std::string& path) {
+    FILE* file = fopen(path.c_str(), "r");
+    if (!file) {
+        std::cerr << "Error opening file: " << path << std::endl;
+        exit(1);
+    }
+    std::vector<JobData> jobs;
+    magnitude_t delta_time, final_time;
+    char universe_file[100];
+    while (fscanf(file, "%s %lf %lf\n", universe_file, &delta_time,
+      &final_time) != EOF) {
+        jobs.push_back({delta_time, final_time, std::string(universe_file)});
+    }
+    std::string directory = path.substr(0, path.find_last_of("/\\"));
+    for (auto& job : jobs) job.directory = directory;
+    fclose(file);
+    return jobs;
+}
 
 int main(int argc, char const *argv[]) {
-  Universe universe(20, 500000);
-  universe.addParticle(new Particle(2500, 5, Vector(0, 0, 0), Vector(0, 0, 0)));
-  universe.addParticle(new Particle(60, 1, Vector(15, 5, 0), Vector(0, 0, 0)));
-  universe.addParticle(new Particle(10000, 20, Vector(120, -30, 0), Vector(0, 0, 0)));
-  universe.addParticle(new Particle(50, 20, Vector(-12, 30, 0), Vector(0, 0, 0)));
-
-  sf::RenderWindow window(sf::VideoMode(1920, 1080), "Particle Simulation");
-
-
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-    if (universe.getCurrentTime() < universe.getFinalTime() && universe.particlesAlive() > 1) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <path_to_jobs_file" << std::endl;
+    return 1;
+  }
+  std::vector<JobData> jobs = loadJobsFromFile(argv[1]);
+  #ifdef INTERFACE
+  for (size_t i = 0; i < jobs.size(); i++) {
+    JobData job = jobs[i];
+    Universe universe(job.delta_time, job.final_time, job.path);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Universe");
+    while (window.isOpen() && universe.particlesAlive() > 1) {
+      sf::Event event;
+      while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+          window.close();
+        }
+      }
       universe.next();
       renderParticles(window, universe.getParticles());
       sleep(0.1);
     }
   }
+  #endif
+
+  #ifndef INTERFACE
+  for (size_t i = 0; i < jobs.size(); i++) {
+    JobData job = jobs[i];
+    Universe universe(job.delta_time, job.final_time, job.path);
+    while (universe.particlesAlive() > 1 && universe.getCurrentTime() < universe.getFinalTime()) {
+      std::cout << "Time: " << universe.getCurrentTime() << std::endl;
+      universe.next();
+    }
+  }
+  #endif
 }
