75922a46bb165495fc1f7ba11b9d095f8bc4ec37
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-12-07 20:46:52 -0600

    Add JobData struct to defines header for job management

--- project2/src/defines.hpp
+++ project2/src/defines.hpp
@@ -1,4 +1,13 @@
 // Copyright Aaron Santana Valdelomar - Univerdidad de Costa Rica 2024
 #pragma once
+#include <string>
+
 #define magnitude_t double
 #define G_CONST 6.67430e-11
+
+struct JobData {
+    magnitude_t delta_time;
+    magnitude_t final_time;
+    std::string path;
+    std::string directory;
+};
