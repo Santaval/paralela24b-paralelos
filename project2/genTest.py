35274980086b79084ca9f3c4c8e06c62ca8edc7e
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-12-07 22:44:09 -0600

    Update job001.txt with new job configurations and add new TSV files for guanacastelandia and carrillolandia

--- project2/genTest.py
+++ project2/genTest.py
@@ -11,10 +11,10 @@ def generar_datos_particulas(n, archivo_salida="particulas.tsv"):
     with open(archivo_salida, "w") as archivo:
         archivo.write("masa\tradio\tx\ty\tz\tvx\tvy\tvz\n")  # Encabezado del archivo
         for _ in range(n):
-            masa = random.randint(1, 10000)  # Masa entre 1 y 10000
-            radio = random.randint(1, 50)   # Radio entre 1 y 50
-            x, y, z = (random.uniform(-1000, 1000) for _ in range(3))  # Posición entre -1000 y 1000
-            vx, vy, vz = (random.uniform(-100, 100) for _ in range(3))  # Velocidad entre -100 y 100
+            masa = random.randint(1, 100000)  # Masa entre 1 y 10000
+            radio = random.randint(1, 5000)   # Radio entre 1 y 50
+            x, y, z = (random.uniform(-10000, 10000) for _ in range(3))  # Posición entre -1000 y 1000
+            vx, vy, vz = (random.uniform(-1, 1) for _ in range(3))  # Velocidad entre -100 y 100
             archivo.write(f"{masa}\t{radio}\t{x:.2f}\t{y:.2f}\t{z:.2f}\t{vx:.2f}\t{vy:.2f}\t{vz:.2f}\n")
 
 if __name__ == "__main__":
